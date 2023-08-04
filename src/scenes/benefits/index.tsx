5a121d2032563950e8aea816bc0900e4081de177
Anabel-Espinoza <anabel.espinoza@gmail.com>
2023-08-04 11:53:42 -0400

    Benefits section finished

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,10 +3,12 @@ import  {
     UserGroupIcon,
     AcademicCapIcon
  } from "@heroicons/react/24/solid"
- import { BenefitType, SelectedPage } from "@/shared/types"
+import { BenefitType, SelectedPage } from "@/shared/types"
 import { motion } from 'framer-motion'
 import HText from "@/shared/HText"
 import Benefit from "./Benefit"
+import ActionButton from "@/shared/ActionButton"
+import BenefitsPageGraphic from '@/assets/BenefitsPageGraphic.png'
 
 const benefits: Array<BenefitType> = [
     {
@@ -58,7 +60,8 @@ const Benefits = ({ setSelectedPage }: Props) => {
                 variants={{
                 hidden: {opacity: 0, x: -50},
                 visible: {opacity: 1, x: 0}
-                }}>
+                }}
+            >
                 <HText>MORE THAN JUST A GYM</HText>
                 <p className='my-5 text-sm'>
                     We provide world class fitness equipment, trainers, and classes to get you to your ultimate fitness goal
@@ -84,6 +87,49 @@ const Benefits = ({ setSelectedPage }: Props) => {
                     />
                 ))}
             </motion.div>
+
+            {/* Graphics and description */}
+            <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+                <img className="mx-auto" alt='benefits-graphic' src={BenefitsPageGraphic}/>
+                <div>
+                    <div>
+                        <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                            <motion.div
+                                initial='hidden'
+                                whileInView='visible'
+                                viewport={{once: true, amount: 0.5}}
+                                transition={{ duration: 0.5 }}
+                                variants={{
+                                hidden: {opacity: 0, x: 50},
+                                visible: {opacity: 1, x: 0}
+                                }}
+                            >
+                                <HText>MILLIONS OF HAPPY MEMBERS GETTING {" "}
+                                    <span className="text-primary-500">FIT</span>
+                                </HText>
+                            </motion.div>
+                        </div>
+                    </div>
+                    <motion.div
+                        initial='hidden'
+                        whileInView='visible'
+                        viewport={{once: true, amount: 0.5}}
+                        transition={{ delay:0.2, duration: 0.5 }}
+                        variants={{
+                        hidden: {opacity: 0, x: 50},
+                        visible: {opacity: 1, x: 0}
+                        }}
+                    >
+                        <p className="my-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="my-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </motion.div>
+                    <div className="relative mt-16">
+                        <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                            <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </motion.div>
     </section>
   )
